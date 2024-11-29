2a27c4697c81622b834168a58c7ff50af3318aca
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2024-11-29 15:53:33 +0100

    feat(screenshot_taker.py): add screenshot taking func

---
+++ src/screenshot_taker.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import sync_playwright
+
+
+def take_screenshot(
+    screenshot_path: str, screenshot_file_extension: str, website_url: str
+):
+
+    screenshot_full_file_path = screenshot_path + "." + screenshot_file_extension
+
+    with sync_playwright() as p:
+        # Launch the browser
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        # Open a website
+        page.goto(website_url)
+
+        # Take a screenshot
+        page.screenshot(path=screenshot_full_file_path)
+
+        print(f"Screenshot saved to {screenshot_full_file_path}")
+
+        # Clean up
+        browser.close()
