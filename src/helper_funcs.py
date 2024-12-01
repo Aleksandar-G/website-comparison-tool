05d132492ed6b86665b1918c9edd39e9b8d07252
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2024-12-01 11:54:26 +0100

    feat: add startup func and fucntionality to watch mutiple websites

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -1,7 +1,17 @@
 from os import listdir
 
+
 def is_folder_empty(folder: str) -> bool:
     if not listdir(folder):
         return True
     else:
         return False
+
+
+class Website:
+    name = ""
+    url = ""
+
+    def __init__(self, name, url) -> None:
+        self.name = name
+        self.url = url
--- src/main.py
+++ src/main.py
@@ -7,45 +7,68 @@ from postprocess_image import convert_screenshot_to_black_white
 from compare_screenshots import compare_screenshots
 
 SCREENSHOT_FOLDER = "./screenshots/"
-SCREENSHOT_FILE_NAME_PREFIX = "ss"
 SCREENSHOT_FILE_EXTENSION = "png"
-WEBSITE_URL = "https://www.pararius.com/apartments/eindhoven/0-1200/1-bedrooms"
 WAIT_PERIOD_SECONDS = 60
 
-screenshot_path = SCREENSHOT_FOLDER + SCREENSHOT_FILE_NAME_PREFIX
-old_screenshot_file_name = screenshot_path + "_old"
-screenshot_full_path = f"{screenshot_path}.{SCREENSHOT_FILE_EXTENSION}"
-screenshot_old_full_path = f"{screenshot_path}_old.{SCREENSHOT_FILE_EXTENSION}"
+websites = [
+    helper_funcs.Website(
+        name="pararius",
+        url="https://www.pararius.com/apartments/eindhoven/0-1200/1-bedrooms",
+    ),
+    helper_funcs.Website(
+        name="funda",
+        url="https://www.funda.nl/zoeken/huur?selected_area=%5B%22eindhoven%22%5D&rooms=%221-%22&price=%22-1250%22&sort=%22date_down%22",
+    ),
+    helper_funcs.Website(
+        name="holland2stay",
+        url="https://holland2stay.com/residences?page=1&available_to_book%5Bfilter%5D=Available+in+lottery%2C336&city%5Bfilter%5D=Eindhoven%2C29",
+    ),
+]
 
 
 def main():
-
-    # Check if that is the first time that the script is running
     if helper_funcs.is_folder_empty(SCREENSHOT_FOLDER):
-        # take 2 screenshots
-        screenshot = take_screenshot(
-            old_screenshot_file_name, SCREENSHOT_FILE_EXTENSION, WEBSITE_URL
-        )
-
-        convert_screenshot_to_black_white(screenshot, screenshot_old_full_path)
-
-        take_screenshot(screenshot_path, SCREENSHOT_FILE_EXTENSION, WEBSITE_URL)
-
-        convert_screenshot_to_black_white(screenshot, screenshot_full_path)
+        for website in websites:
+            startup(website)
     else:
-        # make the "new" screenshot "old" and take a new screenshot
-        rename(screenshot_full_path, screenshot_old_full_path)
-        screenshot = take_screenshot(
-            screenshot_path, SCREENSHOT_FILE_EXTENSION, WEBSITE_URL
-        )
-        # convert_screenshot_to_black_white(screenshot_path, SCREENSHOT_FILE_EXTENSION)
-        convert_screenshot_to_black_white(screenshot, screenshot_full_path)
+        for website in websites:
+            watchdog(website)
+
+
+def startup(website):
+    screenshot_path = SCREENSHOT_FOLDER + website.name
+    screenshot_full_path = f"{screenshot_path}.{SCREENSHOT_FILE_EXTENSION}"
+    old_screenshot_file_name = screenshot_path + "_old"
+    screenshot_old_full_path = f"{screenshot_path}_old.{SCREENSHOT_FILE_EXTENSION}"
+
+    # take 2 screenshots
+    screenshot = take_screenshot(
+        old_screenshot_file_name, SCREENSHOT_FILE_EXTENSION, website
+    )
+
+    convert_screenshot_to_black_white(screenshot, screenshot_old_full_path)
+
+    take_screenshot(screenshot_path, SCREENSHOT_FILE_EXTENSION, website)
+
+    convert_screenshot_to_black_white(screenshot, screenshot_full_path)
+
+
+def watchdog(website: helper_funcs.Website):
+    screenshot_path = SCREENSHOT_FOLDER + website.name
+    screenshot_full_path = f"{screenshot_path}.{SCREENSHOT_FILE_EXTENSION}"
+    screenshot_old_full_path = f"{screenshot_path}_old.{SCREENSHOT_FILE_EXTENSION}"
+
+    # make the "new" screenshot "old" and take a new screenshot
+    rename(screenshot_full_path, screenshot_old_full_path)
+    screenshot = take_screenshot(screenshot_path, SCREENSHOT_FILE_EXTENSION, website)
+
+    convert_screenshot_to_black_white(screenshot, screenshot_full_path)
 
     # compare the screenshots
     if compare_screenshots(screenshot_old_full_path, screenshot_full_path):
-        print("The same")
+        print(f"{website.name} no updates")
     else:
-        print("Different")
+        print(f"{website.name} has new listings")
 
     # send a notification on whatsapp or telegram
 
