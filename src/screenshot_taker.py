d2b2fcf4f390419865d30fe3a0b0ba8380040e60
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2024-12-01 11:53:29 +0100

    feat(screenshot_taker): avoid bot protection and dinamically wait for pages to load

--- src/screenshot_taker.py
+++ src/screenshot_taker.py
@@ -1,24 +1,28 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError
+from helper_funcs import Website
 
 
 def take_screenshot(
-    screenshot_path: str, screenshot_file_extension: str, website_url: str
+    screenshot_path: str, screenshot_file_extension: str, website: Website
 ):
 
     screenshot_full_file_path = screenshot_path + "." + screenshot_file_extension
 
     with sync_playwright() as p:
         # Launch the browser
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
+        try:
+            # Open a website
+            page.goto(website.url, wait_until="networkidle")
 
-        # Open a website
-        page.goto(website_url)
+            # Take a screenshot
+            screenshot = page.screenshot(path=screenshot_full_file_path)
 
-        # Take a screenshot
-        screenshot = page.screenshot(path=screenshot_full_file_path)
+        except TimeoutError:
+            screenshot = page.screenshot(path=screenshot_full_file_path)
 
-        print(f"Screenshot taken")
+        print(f"Screenshot for {website.name} taken")
 
         # Clean up
         browser.close()
